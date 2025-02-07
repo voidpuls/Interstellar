4e2d3b78f745c67e73debbbab6f7a8141f066baf
voidpuls <catboomfat@gmail.com>
2025-02-07 09:46:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,10 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    Voidpulse: "Sunset2006!",
     mouse: "squeak39",
     sloan: "fortnite1",
+    potato: "potato",
   },
 };
 
